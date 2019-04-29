1e9ea75122ee99c9068307ace2d0b0b885981450
tianhao <1010280411@qq.com>
2019-04-29 11:28:32 +0800

    过滤js脚本

--- src/main/java/com/youzhi/util/BulletProofUtil.java
+++ src/main/java/com/youzhi/util/BulletProofUtil.java
@@ -16,5 +16,17 @@ public class BulletProofUtil {
 	public static String antiSQLInjection(String msg) {
 		return StringEscapeUtils.escapeSql(msg); 
 	}
+	
+	/**
+	 * 过滤js脚本 (识别<script>)
+	 */
+	public static String filterJSScript(String msg) {
+		return StringEscapeUtils.escapeHtml(msg); 
+	}
+	
+	public static void main(String[] args) {
+		System.out.println(filterJSScript("<script>alert('hello world'); </script>"));
+	}
+	
 
 }
